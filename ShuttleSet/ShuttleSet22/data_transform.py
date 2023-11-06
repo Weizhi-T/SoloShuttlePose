8e85d0a672f615f68f8773c063954153f28b443a
sunwuzhou03 <sunwuzhou03@gmail.com>
2023-11-06 14:01:53 +0800

    update match dataset

--- ShuttleSet/ShuttleSet22/data_transform.py
+++ ShuttleSet/ShuttleSet22/data_transform.py
@@ -10,10 +10,11 @@ def split_data_by_match_and_rally(shot_path,match_dict):
         folder_path = f'ShuttleSet/ShuttleSet22/match/{video}'
         os.makedirs(folder_path, exist_ok=True)
 
-        rallies = match_data.groupby('rally')
+        rallies = match_data.groupby('rally_id')
+        rid=1
         for rally_id, rally_data in rallies:
-            rally_filename = f'rally_{rally_id}.csv'
-            
+            rally_filename = f'rally_{rid}.csv'
+            rid+=1 
             rally_data.to_csv(os.path.join(folder_path, rally_filename), index=False)
 
 shot_path = 'ShuttleSet\ShuttleSet22\set\shot_metadata.csv'  
